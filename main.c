7af214caa0baf87f9f28086e860b7e19b31e946a
audi <otiaudi9@gmail.com>
2022-11-11 13:50:01 +0300

    my header file

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+int main() {
+    while (true)
+    {
+        printf("> ");
+        char *line =read_line();
+}
+	//        char **tokens = kash_split_line(line);
+
+//        if (tokens[0] != NULL) {
+  //          kash_exec(tokens);
+      //  }
+
+    //    free(tokens);
+        //free(line);
+    //}
+ }
