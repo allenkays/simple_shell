eadf0814dbe284366afebd13c8eebd0b5e4e0c52
allenkays <allenkays@tutanota.com>
2022-11-11 06:05:09 -0500

    rename exec funtion

--- exec.c
+++ exec.c
@@ -1,12 +1,12 @@
 #include <main.h>
 
 /**
- * exec_penv - executes given command or launch program
+ * alx_exec- executes given command or launch program
  * args:- pointer to pointer to chars params
  *
  * return: 1 on success -1 0r error on failure
  */
-void exec_penv(char **args)
+void alx_exec(char **args)
 {
 	pid_t child_pid = fork();
 	pid_t wpid;
