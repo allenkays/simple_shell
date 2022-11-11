6b73796045a5ec289facdacf0878548d3507ad0f
allenkays <allenkays@tutanota.com>
2022-11-11 06:11:28 -0500

    remove header file

---
+++ exit_builtin.c
@@ -0,0 +1,8 @@
+/**
+ * exit_builtin - exits graciously
+ * args: double pointer to character arguments.
+ */
+void exit_builtin(void)
+{
+	exit(0);
+}
--- main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#define clear() printf("\033[H\033[J")
-#endif
