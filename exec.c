c0a3aa6ca7636c700e8ad0541401855788110b44
allenkays <allenkays@tutanota.com>
2022-11-11 05:20:33 -0500

    create execute function

---
+++ exec.c
@@ -0,0 +1,36 @@
+#include <main.h>
+
+/**
+ * exec_penv - executes given command or launch program
+ * args:- pointer to pointer to chars params
+ *
+ * return: 1 on success -1 0r error on failure
+ */
+void exec_penv(char **args)
+{
+	pid_t child_pid = fork();
+	pid_t wpid;
+	int status;
+
+	if (child_pid == 0)
+	{
+		/**
+		 * execvp - executes program within enviroment path
+		 * args: arguments to command or program
+		 */
+		execvp(args[0], args);
+		perror("hsh");
+		exit(EXIT_FAILURE);
+	}
+	else if (child_pid < 0)
+	{
+		perror("hsh");
+	}
+	else
+	{
+		do {
+			waitpid(child_pid, &status, WUNRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return (1);
+}
---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#define clear() printf("\033[H\033[J")
+#endif
