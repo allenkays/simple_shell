1d7bf185b92371b1794e4a90aa1b841b4fd728a4
allenkays <allenkays@tutanota.com>
2022-11-16 09:41:25 -0500

    variable checks

---
+++ alx_rep_var.c
@@ -0,0 +1,183 @@
+#include "shell.h"
+
+/**
+ * check_env - checks if the typed variable is an env variable
+ *
+ * @h: head of linked list
+ * @in: input string
+ * @parameters: parameters structure
+ * Return: no return
+ */
+void check_env(varz_list **h, char *in, input_params *parameters)
+{
+	int row, chr, j, lval;
+	char **_envr;
+
+	_envr = parameters->_environ;
+	for (row = 0; _envr[row]; row++)
+	{
+		for (j = 1, chr = 0; _envr[row][chr]; chr++)
+		{
+			if (_envr[row][chr] == '=')
+			{
+				lval = _strlen(_envr[row] + chr + 1);
+				add_rvar_node(h, j, _envr[row] + chr + 1, lval);
+				return;
+			}
+
+			if (in[j] == _envr[row][chr])
+				j++;
+			else
+				break;
+		}
+	}
+
+	for (j = 0; in[j]; j++)
+	{
+		if (in[j] == ' ' || in[j] == '\t' || in[j] == ';' || in[j] == '\n')
+			break;
+	}
+
+	add_rvar_node(h, j, NULL, 0);
+}
+
+/**
+ * check_vars - check if the typed variable is $$ or $?
+ *
+ * @h: head of the linked list
+ * @in: input string
+ * @st: last status of the Shell
+ * @parameters: parameters structure
+ * Return: no return
+ */
+int check_vars(varz_list **h, char *in, char *st, input_params *parameters)
+{
+	int i, lst, lpd;
+
+	lst = _strlen(st);
+	lpd = _strlen(parameters->pid);
+
+	for (i = 0; in[i]; i++)
+	{
+		if (in[i] == '$')
+		{
+			if (in[i + 1] == '?')
+				add_rvar_node(h, 2, st, lst), i++;
+			else if (in[i + 1] == '$')
+				add_rvar_node(h, 2, parameters->pid, lpd), i++;
+			else if (in[i + 1] == '\n')
+				add_rvar_node(h, 0, NULL, 0);
+			else if (in[i + 1] == '\0')
+				add_rvar_node(h, 0, NULL, 0);
+			else if (in[i + 1] == ' ')
+				add_rvar_node(h, 0, NULL, 0);
+			else if (in[i + 1] == '\t')
+				add_rvar_node(h, 0, NULL, 0);
+			else if (in[i + 1] == ';')
+				add_rvar_node(h, 0, NULL, 0);
+			else
+				check_env(h, in + i, parameters);
+		}
+	}
+
+	return (i);
+}
+
+/**
+ * replaced_input - replaces string into variables
+ *
+ * @head: head of the linked list
+ * @input: input string
+ * @new_input: new input string (replaced)
+ * @nlen: new length
+ * Return: replaced string
+ */
+char *replaced_input(varz_list **head, char *input, char *new_input, int nlen)
+{
+	varz_list *indx;
+	int i, j, k;
+
+	indx = *head;
+	for (j = i = 0; i < nlen; i++)
+	{
+		if (input[j] == '$')
+		{
+			if (!(indx->len_var) && !(indx->len_val))
+			{
+				new_input[i] = input[j];
+				j++;
+			}
+			else if (indx->len_var && !(indx->len_val))
+			{
+				for (k = 0; k < indx->len_var; k++)
+					j++;
+				i--;
+			}
+			else
+			{
+				for (k = 0; k < indx->len_val; k++)
+				{
+					new_input[i] = indx->val[k];
+					i++;
+				}
+				j += (indx->len_var);
+				i--;
+			}
+			indx = indx->next;
+		}
+		else
+		{
+			new_input[i] = input[j];
+			j++;
+		}
+	}
+
+	return (new_input);
+}
+
+/**
+ * rep_var - calls functions to replace string into vars
+ *
+ * @input: input string
+ * @datash: parameters structure
+ * Return: replaced string
+ */
+char *rep_var(char *input, input_params *datash)
+{
+	varz_list *head, *indx;
+	char *status, *new_input;
+	int olen, nlen;
+
+	status = aux_itoa(datash->status);
+	head = NULL;
+
+	olen = check_vars(&head, input, status, datash);
+
+	if (head == NULL)
+	{
+		free(status);
+		return (input);
+	}
+
+	indx = head;
+	nlen = 0;
+
+	while (indx != NULL)
+	{
+		nlen += (indx->len_val - indx->len_var);
+		indx = indx->next;
+	}
+
+	nlen += olen;
+
+	new_input = malloc(sizeof(char) * (nlen + 1));
+	new_input[nlen] = '\0';
+
+	new_input = replaced_input(&head, input, new_input, nlen);
+
+	free(input);
+	free(status);
+	free_rvar_list(&head);
+
+	return (new_input);
+}
